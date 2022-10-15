aba916f6b7810401d79696c461c10da971e77bca
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-10-15 11:06:03 +0530

    update

---
+++ assets/Resources/Resume-CV/assets/js/main.js
@@ -0,0 +1,4 @@
+const messanger = document.querySelector('.avatar__block')
+messanger.querySelector('span').addEventListener('click', () => {
+    window.open('https://t.me/shubham996633', '_blank')
+})
