d3731addde77b733c9ef70bfbec60e0bf0148eb8
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-10-16 08:01:50 +0530

    update

--- assets/Resources/Resume-CV/assets/js/main.js
+++ assets/Resources/Resume-CV/assets/js/main.js
@@ -2,3 +2,12 @@ const messanger = document.querySelector('.avatar__block')
 messanger.querySelector('span').addEventListener('click', () => {
     window.open('https://t.me/shubham996633', '_blank')
 })
+
+const bars = document.querySelectorAll('.progress__bar')
+
+bars.forEach(function(bar){
+    let percentage = bar.dataset.percent
+    let tooltip = bar.children[0]
+    tooltip.innerText = percentage +'%'
+    bar.style.width = percentage + '%'
+})
