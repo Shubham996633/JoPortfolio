d14f9d7dd036faa84ef3a6b8cc544669d838d4e9
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-10-17 13:50:31 +0530

    update

--- assets/Resources/test/Responsive__Portfolio__Website-master/assets/js/main.js
+++ assets/Resources/test/Responsive__Portfolio__Website-master/assets/js/main.js
@@ -160,7 +160,7 @@ window.addEventListener('scroll', scrollHeader)
 function scrollUp(){
   const scrollUp = document.getElementById('scroll-up');
   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-up class
-  if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+  if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
