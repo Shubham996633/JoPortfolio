219fc11903fdca7e78bd07b196ef039ddffe088b
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-10-18 20:14:35 +0530

    update

--- assets/Resources/Portfolio Testing/assets/js/main.js
+++ assets/Resources/Portfolio Testing/assets/js/main.js
@@ -237,3 +237,44 @@ function init() {
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
 }
+
+
+var mixerPortfolio = mixitup('.work__container', {
+  selectors: {
+      target: '.work__card'
+  },
+  animation: {
+      duration: 300
+  }
+});
+
+
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork(){
+  linkWork.forEach(l => {
+      l.classList.remove('active-work')
+      this.classList.add('active-work')
+  })
+}
+
+linkWork.forEach(l=> l.addEventListener('click', activeWork))
+
+document.addEventListener('click', (e) => {
+  if(e.target.classList.contains('work__button')){
+      togglePortfolioPopup()
+      portfolioItemDetails(e.target.parentElement)
+  }
+})
+
+function togglePortfolioPopup(){
+  document.querySelector('.portfolio__popup').classList.toggle('open')
+}
+
+document.querySelector('.portfolio__popup-close').addEventListener('click', togglePortfolioPopup)
+
+function portfolioItemDetails(portfolioItem){
+  document.querySelector('.portfolio__popup-img').src = portfolioItem.querySelector('.work__img').src
+  document.querySelector('.portfolio__popup-subtitle span').innerHTML = portfolioItem.querySelector('.work__title').innerHTML
+  document.querySelector('.portfolio__popup-body').innerHTML = portfolioItem.querySelector('.portfolio__item-details').innerHTML
+}
