3fd4bc9e891035470f0b3720f3bd5a2891ba79e7
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-10-18 22:18:08 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -290,7 +290,7 @@ const sr = ScrollReveal({
 
 sr.reveal(`.home__social, home__title, button--flex`)
 
-sr.reveal(`.home__description, home__data, .nav`, {delay:500})
+sr.reveal(`.home__description, home__data`, {delay:500})
 sr.reveal(`.skills, .qualification`, {delay:500})
 sr.reveal(`.button--flex, .home__scroll-button, .home__scroll`, {delay:900})
 sr.reveal(`.work__filters, .portfolio__title-heading, .footer, .project__container`, {delay:700})
